f98eb1e36f81848b2133522f56c99d76d9eeeb78
symon <symon1006@naver.com>
2026-05-31 13:32:29 +0900

    feat: requirements.txt 생성 및 settings.py 수정

--- be/config/settings.py
+++ be/config/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -53,10 +54,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 LANGUAGE_CODE = 'ko-kr'
